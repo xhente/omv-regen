fd45fe7e7f3b8f727aca0e57c2e91b468598dac9
xhente <omvforochente@gmail.com>
2023-04-16 02:32:41 +0200

    variables y cambios varios

--- omv-regen.sh
+++ omv-regen.sh
@@ -6,12 +6,10 @@ Rege=""
 Ruta=""
 OpDias=7
 OpExpz=""
-OpForz=""
 OpKern=1
 declare -A Bp
 Bp[VersPlug]='/VersionPlugins'
 Bp[VersKern]='/VersionKernel'
-Bp[VersOMV]='/VersionOMV'
 Bp[Config]='/etc/openmediavault/config.xml'
 Bp[Passwd]='/etc/passwd'
 Bp[Shadow]='/etc/shadow'
@@ -19,19 +17,14 @@ declare -a Origen=( "/home" "/etc/libvirt/qemu" "/etc/wireguard" )
 Fecha=$(date +%y%m%d_%H%M)
 VersionOR=""
 VersionDI=""
-InstaladoII=""
-Instalado=""
-Versiones=""
+InstII=""
+VersIdem=""
 confCmd="omv-salt deploy run"
 cont=0
-sucio=0
-Kernel=""
-ZFS=""
-Compose=""
-Shareroot=""
-Apttool=""
 declare -a ListaInstalar
-declare -a ListaDesinst
+VersionKernel=""
+URL="https://github.com/OpenMediaVault-Plugin-Developers/packages/raw/master"
+confCmd="omv-salt deploy run"
 
 [ "$(cut -b 7,8 /etc/default/locale)" = es ] && Sp=1
 echoe () { [ ! "$Sp" ] && echo -e "$1" || echo -e "$2"; }
@@ -42,9 +35,10 @@ help () {
   echo -e "                                                                                       "
   echo -e "  - omv-regen regenerates an OMV system from a clean install by restoring the existing "
   echo -e "  configurations to the original system.                                               "
-  echo -e "  - Install omv-regen on the original system and make a backup, then install OMV on an "
-  echo -e "  empty disk without configuring anything, then install omv-regen and mount the backup "
-  echo -e "  on a flash drive, then regenerate. The version available on the internet must match. "
+  echo -e "  - Update the system. Install omv-regen on the original system and make a backup. Then"
+  echo -e "  install OMV on an empty disk without configuring anything. Then Install omv-regen and"
+  echo -e "  mount the backup on a flash drive, then regenerate.  The version available on the in-"
+  echo -e "  ternet must match.                                                                   "
   echo -e "  - Use omv-regen backup      to store the necessary information to regenerate.        "
   echo -e "  - Use omv-regen regenerate  to run a system regeneration from a clean install of OMV."
   echo -e "  - This script must be executed as root or using sudo.                                "
@@ -65,22 +59,29 @@ help () {
   echo -e "                                                                                       "
   echo -e "      Options and parameters:                                                          "
   echo -e "        PATH_TO_BACKUP  Path where the backup is stored with the data to regenerate.   "
-  echo -e "              -f        Force regeneration without version matching.                   "
-  echo -e "                        It is only possible if the difference is minimal.              "
-  echo -e "                        Its use is not recommended. A new backup is preferable.        "
   echo -e "              -k        Skip installing the proxmox kernel.                            "
   echo -e "                                                                                       "
   exit
 }
 
-# Analizar estado de plugin. Versiones y instalación
+# Analizar estado de plugin. Instalación y versiones.
 Analiza () {
-  VersionOR=""; VersionDI=""; InstaladoII=""
+  VersionOR=""
+  VersionDI=""
+  InstII=""
   VersionOR=$(awk -v i="$1" '$1 == i {print $2}' "${Ruta}${Bp[VersPlug]}")
   VersionDI=$(apt-cache madison "$1" | awk '{print $3}')
-  InstaladoII=$(dpkg -l | awk -v i="$1" '$2 == i { print $1 }')
-  if [ "${InstaladoII}" == "ii" ]; then Instalado=SI; else Instalado=NO; fi
-  if [ "${VersionOR}" = "${VersionDI}" ]; then Versiones=SI; else Versiones=NO; fi
+  InstII=$(dpkg -l | awk -v i="$1" '$2 == i { print $1 }')
+  if [ "${InstII}" == "ii" ]; then
+    InstII=OK
+  else
+    InstII=NO
+  fi
+  if [ "${VersionOR}" = "${VersionDI}" ]; then
+    VersIdem=OK
+  else
+    VersIdem=NO
+  fi
 }
 
 # Instalar complemento
@@ -90,6 +91,12 @@ InstalaPlugin () {
     echoe "Failed to install $1 plugin." "No se pudo instalar el complemento $1."
     "${confCmd}" "$1"
     apt-get --yes --fix-broken install
+    if [ $OpForz ]; then
+      echoe "TRADUCCION" "Opción Forzar activada, se continúa la regeneración."
+    else
+      echoe "Exiting..." "Saliendo..."
+      exit
+    fi
   fi
 }
 
@@ -118,8 +125,14 @@ elif [[ $1 == "regenera" ]]; then
 else  
   help
 fi
-[ -d "${2}" ] && Ruta="$2" || help
-shift 2
+
+if [ -d "${2}" ]; then
+  Ruta="$2"
+  shift 2
+else
+  echoe "TRADUCCION" "La ruta $2 no existe. Saliendo..."
+  help
+fi
 
 if [ $Back ]; then
   while getopts "d:ehv" opt; do
@@ -136,27 +149,25 @@ if [ $Back ]; then
         help
         ;;
       *)
-        echoe "Invalid command." "Comando inválido."
+        echoe "Invalid argument. Exiting..." "Argumento inválido. Saliendo..."
         help
         ;;
     esac
   done
-else
+fi
+
+if [ $Rege ]; then
   while getopts "fhkv" opt; do
     case "$opt" in
-      f)
-        OpForz=1
-        echoe "TRADUCCION" "Se forzará la regeneración si las versiones no coinciden"
-        ;;
       h)
         help
         ;;
       k)
         OpKern=""
-        echoe "TRADUCCION" "No se instalará el kernel proxmox"
+        echoe "TRADUCCION" "No se instalará el kernel proxmox."
         ;;
       *)
-        echoe "Invalid command." "Comando inválido."
+        echoe "Invalid argument. Exiting..." "Argumento inválido. Saliendo..."
         help
         ;;
     esac
@@ -165,14 +176,14 @@ fi
 
 shift $((OPTIND -1))
 if [[ -n "$Rege" && -n $1 ]]; then
-  echoe "Invalid command." "Comando inválido."
+  echoe "$1 Invalid argument. Exiting..." "$1 Argumento inválido. Saliendo..."
   help
 else
   for i in "$@"; do
     if [ -d "$i" ]; then
       echoe "TRADUCCION" "La carpeta $i se va a copiar."
     else  
-      echoe "TRADUCCION" "La carpeta $i no existe o no es accesible."
+      echoe "TRADUCCION" "La carpeta $i no existe o no es accesible. Saliendo..."
       help
     fi
   done
@@ -196,9 +207,6 @@ if [ $Back ]; then
   echoe ">>>    Extracting kernel version..." ">>>    Extrayendo versión del kernel..."
   uname -r | tee "${Destino}${Bp[VersKern]}"
 
-  echoe ">>>    Extracting OMV version..." ">>>    Extrayendo versión de OMV..."
-  dpkg -l | awk '$2 == "openmediavault" { print $2" "$3 }'| tee "${Destino}${Bp[VersOMV]}"
-
   for i in "${Origen[@]}"; do
     if [ -d "$i" ]; then
       echoe ">>>    Copying data from $i..." ">>>    Copiando datos de $i..."
@@ -214,7 +222,7 @@ if [ $Back ]; then
     shift
   done
 
-  echoe ">>>    Deleting backups larger than ${Dias} days..." ">>>    Eliminando backups de más de ${Dias} días..."
+  echoe ">>>    Deleting backups larger than ${Dias} days..." ">>>    Eliminando backups de hace más de ${Dias} días..."
   find "${Ruta}/omv-regen/" -maxdepth 1 -type d -name "ORB_*" -mtime "+$OpDias" -exec rm -rv {} +
   # -mmin = minutos  ///  -mtime = dias
   
@@ -246,20 +254,20 @@ done
 
 # Versión de OMV original
 Analiza "openmediavault"
-if [ "${Versiones}" = "NO" ]; then
+if [ "${VersIdem}" = "NO" ]; then
   echoe "TRADUCCION" "La versión de OMV del servidor original no coincide.  Saliendo..."
   help
 fi
 
 # Actualizar sistema
 if ! omv-upgrade; then
-  echoe "Failed updating system" "Error actualizando el sistema.  Saliendo..."
-  help
+  echoe "Failed updating system. Exiting..." "Error actualizando el sistema.  Saliendo..."
+  exit
 fi
 
 # Instalar omv-extras si existía y no está instalado
 Analiza "openmediavault-omvextrasorg"
-if [ "${Versiones}" = "SI" ] && [ "${Instalado}" = "NO" ]; then
+if [ "${VersionOR}" ] && [ "${InstII}" = "NO" ]; then
   echoe "Downloading omv-extras.org plugin for openmediavault 6.x ..." "Descargando el complemento omv-extras.org para openmediavault 6.x ..."
   File="openmediavault-omvextrasorg_latest_all6.deb"
   if [ -f "${File}" ]; then
@@ -271,38 +279,38 @@ if [ "${Versiones}" = "SI" ] && [ "${Instalado}" = "NO" ]; then
       echoe "Installing other dependencies ..." "Instalando otras dependencias ..."
       apt-get --yes --fix-broken install
       Analiza "openmediavault-omvextrasorg"
-      if [[ "${Instalado}" = "NO" ]]; then
+      if [[ "${InstII}" = "NO" ]]; then
         echoe "omv-extras failed to install correctly.  Trying to fix with ${confCmd} ..." "omv-extras no se pudo instalar correctamente. Intentando corregir con ${confCmd} ..."
         if "${confCmd}" omvextras; then
           echoe "Trying to fix apt ..." "Tratando de corregir apt..."
           apt-get --yes --fix-broken install
         else
-          echoe "${confCmd} failed and openmediavault-omvextrasorg is in a bad state." "${confCmd} falló y openmediavault-omvextrasorg está en mal estado."
+          echoe "${confCmd} failed and openmediavault-omvextrasorg is in a bad state. Exiting..." "${confCmd} falló y openmediavault-omvextrasorg está en mal estado. Saliendo..."
           exit 3
         fi
       fi
       Analiza "openmediavault-omvextrasorg"
-      if [[ "${Instalado}" = "NO" ]]; then
-        echoe "openmediavault-omvextrasorg package failed to install or is in a bad state." "El paquete openmediavault-omvextrasorg no se pudo instalar o está en mal estado."
+      if [[ "${InstII}" = "NO" ]]; then
+        echoe "openmediavault-omvextrasorg package failed to install or is in a bad state. Exiting..." "El paquete openmediavault-omvextrasorg no se pudo instalar o está en mal estado. Saliendo..."
         exit 3
       fi
     fi
     echoe "Updating repos ..." "Actualizando repositorios..."
     "${confCmd}" omvextras
   else
-    echoe "There was a problem downloading the package." "Hubo un problema al descargar el paquete."
+    echoe "There was a problem downloading the package. Exiting..." "Hubo un problema al descargar el paquete. Saliendo..."
     exit
   fi
 fi
 
 # Analizar versiones y complementos especiales
-cont=0; sucio=0
+cont=0
 for i in $(awk '{print NR}' "${Ruta}${Bp[VersPlug]}")
 do
   Plugin=$(awk -v i="$i" 'NR==i{print $1}' "${Ruta}${Bp[VersPlug]}")
   Analiza "${Plugin}"
-  if [ "${Instalado}" = "NO" ]; then
-    echoe "TRADUCCION" "Versiones $Versiones \tInstalado $Instalado \t${Plugin} \c"
+  if [ "${InstII}" = "NO" ]; then
+    echoe "Versions $VersIdem \tInstalled $InstII \t${Plugin} \c" "Versiones $VersIdem \tInstalado $InstII \t${Plugin} \c"
     case "${Plugin}" in
       *"kernel" ) ;;
       *"zfs" ) ;;
@@ -312,74 +320,43 @@ do
         ListaInstalar[cont]="${Plugin}"
         ;;
     esac
-    # Marcar complementos sucios. Desinstalar al final
-    if [ "${Versiones}" = "NO" ]; then
-      # AÑADIR CONDICION. SI LA DIFERENCIA DE VERSION ES FUERTE CONDICIONAR A $OpForz
-      # Si la primera columna de version es igual y $OpForz=1 
-      #   Instalar
-      # sino
-      #   echoe "TRADUCCION" "Las versiones no coinciden. Saliendo..."
-      #   help
-      #fi
-      #
-      echoe "TRADUCCION" "\t**********\n********** ERROR:  Las versiones no coinciden  -->  Este complemento no se instalará."
-      case "${Plugin}" in
-        *"kernel" ) Kernel=11 ;;
-        *"zfs" ) ZFS=11 ;;
-        *"compose" ) Compose=11 ;;
-        *"sharerootfs" ) Shareroot=11 ;;
-        *"apttool" ) Apttool=11 ;;
-        * )
-          (( sucio++ ))
-          ListaDesinst[$sucio]="${Plugin}"
-          ;;
-      esac
+    if [ "${VersIdem}" = "NO" ]; then
+      echoe "TRADUCCION" "\t**********\n********** ERROR:  Versión disponible $VersionDI La versión del sistema original $VersionOR no coincide. Saliendo..."
+      help
     else
-      echoe "TRADUCCION" "  -->  Se va a instalar"
+      echoe "TRADUCCION" "  -->  Se va a instalar..."
     fi
   fi
 done
 
-# Opción forzar
-if [ ! $OpForz ]; then
-  # AÑADIR CONDICION. SI LA DIFERENCIA DE VERSION ES FUERTE CONDICIONAR A $OpForz
-      # Si la primera columna de version es igual y $OpForz=1 
-      #   Instalar
-      # sino
-      #   echoe "TRADUCCION" "Las versiones no coinciden. Saliendo..."
-      #   help
-      #fi
-  echoe "TRADUCCION" "No se puede realizar la regeneración. Hay versiones de paquetes no coincidentes con el servidor de origen."
-  help
-fi
-
 # Instalar openmediavault-kernel
-Analizar openmediavault-kernel
-if [ "${Versiones}" = SI ] && [ "${Instalado}" = "NO" ]; then
+Analiza openmediavault-kernel
+if [ "${VersIdem}" = "OK" ] && [ "${InstII}" = "NO" ]; then
   InstalaPlugin openmediavault-kernel
 fi
 
-# Instalar Kernel proxmox si omv-kernel no está sucio
-if  [ ! "${Kernel}" = "11" ] && [ $OpKern ]; then
-  VersKernelOri=$(awk -F "." '/pve$/ {print $1"."$2 }' "${Ruta}${Bp[VersKern]}")
-  VersionKernelIns=$(uname -r | awk -F "." '/pve$/ {print $1"."$2 }')
-  if [ "${VersKernelOri}" ] && [ ! "${VersKernelOri}" = "${VersionKernelIns}" ]; then
+# Instalar Kernel proxmox
+if [ $OpKern ]; then
+  KernelOR=$(awk -F "." '/pve$/ {print $1"."$2 }' "${Ruta}${Bp[VersKern]}")
+  KernelIN=$(uname -r | awk -F "." '/pve$/ {print $1"."$2 }')
+  if [ "${KernelOR}" ] && [ ! "${KernelOR}" = "${KernelIN}" ]; then
     echoe "Installing proxmox kernel" "Instalando kernel proxmox"
-    source /usr/sbin/omv-installproxmox "${VersKernelOri}"
+    source /usr/sbin/omv-installproxmox "${KernelOR}"
     #
     #
     # SOLUCIONAR EXIT AL FINAL DEL PROGRAMA LLAMADO
     #
     #
-    [ ! -f /etc/cron.d/regen-reboot ] && touch /etc/cron.d/regen-reboot
-    echo "@ reboot root /home/omv-regen-regenera.sh $Ruta" >> /etc/cron.d/regen-reboot
+    [ ! -f /etc/cron.d/omvregenreboot ] && touch /etc/cron.d/omvregenreboot
+    # Si se añaden otras opciones al menú regenera se deben añadir aquí también las variables
+    echo "@ reboot omv-regen regenera $Ruta" >> /etc/cron.d/omvregenreboot
     reboot
   fi
 fi
 
 # Instalar openmediavault-zfs. Importar pools.
 Analiza openmediavault-zfs
-if [ "${Versiones}" = SI ] && [ "${Instalado}" = "NO" ]; then
+if [ "${VersIdem}" = OK ] && [ "${InstII}" = "NO" ]; then
   InstalaPlugin openmediavault-zfs
   if ! zpool import -a; then
     zpool import -f
@@ -413,7 +390,7 @@ omv-salt stage run deploy
 
 # Reinstalar openmediavault-sharerootfs
 Analiza sharerootfs
-if [ "$Instalado" = "SI" ]; then
+if [ "$InstII" = "OK" ]; then
   source /usr/share/openmediavault/scripts/helper-functions
   uuid="79684322-3eac-11ea-a974-63a080abab18"
   if [ "$(omv_config_get_count "//mntentref[.='${uuid}']")" = "0" ]; then
